63eacbe49e886b38efc0d8466679efd49c23fc2c
Walter <wxfwalter@gmail.com>
2017-10-22 16:50:39 +0800

    finish everything about user

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.feature "Users", type: :feature do
+  scenario 'user creates an account' do
+    visit register_path
 
+    user_params = Fabricate.attributes_for(:user)
+
+    fill_in 'Email', with: user_params[:email]
+    fill_in 'Password', with: user_params[:password]
+    fill_in 'First name', with: user_params[:first_name]
+    fill_in 'Last name', with: user_params[:last_name]
+
+    click_button 'Sign Up'
+    expect(page).to have_content 'Created an account successfully!'
+  end
 end
