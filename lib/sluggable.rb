61ff50bfd4d02a06a4c714e3c5b482ebfb4d7554
Walter <wxfwalter@gmail.com>
2017-10-27 10:58:04 +0800

    fix sluggable spec bugs

--- lib/sluggable.rb
+++ lib/sluggable.rb
@@ -2,16 +2,19 @@ module Sluggable
   extend ActiveSupport::Concern
 
   included do
-    before_save :generate_slug!
+    after_validation :generate_slug!
     class_attribute :slug_column
   end
 
   def to_slug
+    binding.pry
     slug = self.send(self.class.slug_column).gsub(/[^a-zA-Z0-9]/, '-').downcase
     slug.gsub(/[-]+/, '-')
   end
 
   def generate_slug!
+    # return self.slug = 'test' if Rails.env == 'test'
+
     slug = to_slug
     a_regex = /\A#{slug}[\-0-9]*\z/
     count = self.class.select { |ele| ele.slug && ele.slug.match(a_regex)}.size
