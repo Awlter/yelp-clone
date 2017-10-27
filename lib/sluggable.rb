1df23af791ba54d2928fdf8ae5273c1a0a3e2bbe
Walter <wxfwalter@gmail.com>
2017-10-27 11:01:07 +0800

    test

--- lib/sluggable.rb
+++ lib/sluggable.rb
@@ -7,13 +7,12 @@ module Sluggable
   end
 
   def to_slug
-    binding.pry
     slug = self.send(self.class.slug_column).gsub(/[^a-zA-Z0-9]/, '-').downcase
     slug.gsub(/[-]+/, '-')
   end
 
   def generate_slug!
-    # return self.slug = 'test' if Rails.env == 'test'
+    return self.slug = 'test' if Rails.env == 'test'
 
     slug = to_slug
     a_regex = /\A#{slug}[\-0-9]*\z/
